9c8723ca3e706172b12a9c033f5050f1a41024a2
liams-code <99477913+liams-code@users.noreply.github.com>
2022-04-08 22:17:32 +0900

    Update EOS_AI.py

--- EOS_AI.py
+++ EOS_AI.py
@@ -72,7 +72,7 @@ while True:
         schedule.run_pending()
         
         current_price = get_current_price("KRW-EOS")
-        avg_buy_price = get_buy_average("KRW-EOS")
+        avg_buy_price = get_avg_buy_price("KRW-EOS")
         if current_price > (avg_buy_price*1.1) or current_price < (avg_buy_price*0.94):
             eos = get_balance("EOS")
             if eos > 1.6:
