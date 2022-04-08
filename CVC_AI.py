cb1d2c74efa9fbcd6c6b59c068f7b52d16502304
liams-code <99477913+liams-code@users.noreply.github.com>
2022-04-08 22:13:55 +0900

    Update CVC_AI.py

--- CVC_AI.py
+++ CVC_AI.py
@@ -72,7 +72,7 @@ while True:
         schedule.run_pending()
         
         current_price = get_current_price("KRW-CVC")
-        avg_buy_price = get_buy_average("KRW-CVC")
+        avg_buy_price = get_avg_buy_price("KRW-CVC")
         if current_price > (avg_buy_price*1.12) or current_price < (avg_buy_price*0.94):
             cvc = get_balance("CVC")
             if cvc > 14:
