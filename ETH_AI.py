2b9be745d6102e7ebfa33d042f5cef5464b64fec
liams-code <99477913+liams-code@users.noreply.github.com>
2022-08-15 18:46:13 +0900

    Update ETH_AI.py

--- ETH_AI.py
+++ ETH_AI.py
@@ -2,7 +2,8 @@ import time
 import pyupbit
 import datetime
 import schedule
-from fbprophet import Prophet
+import pandas as pd
+from prophet import Prophet
 
 access = "you"
 secret = "you"
@@ -66,31 +67,33 @@ print("autotrade start")
 # 자동매매 시작 , ETH 5000원 이상으로 바꿈 , seconds을 hours=1로 바꿔서 8시에 매도하게 함. break, 10/3,0.3
 while True:
     try:
+        tk = "KRW-ETH"
+        tka = "ETH"
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-ETH")
+        start_time = get_start_time("{}".format(tk))
         end_time = start_time + datetime.timedelta(days=1)
         schedule.run_pending()
         
-        current_price = get_current_price("KRW-ETH")
-        avg_buy_price = get_avg_buy_price("KRW-ETH")
+        current_price = get_current_price("{}".format(tk))
+        avg_buy_price = get_avg_buy_price("{}".format(tk))
         if current_price > (avg_buy_price*1.1) or current_price < (avg_buy_price*0.97):
-            eth = get_balance("ETH")
+            eth = get_balance("{}".format(tka))
             if eth > 0.003:
-                upbit.sell_market_order("KRW-ETH", eth*0.9995)
+                upbit.sell_market_order("{}".format(tk), eth*0.9995)
                 break
 
         if start_time < now < end_time - datetime.timedelta(hours=1):
-            target_price = get_target_price("KRW-ETH", 0.3)
-            current_price = get_current_price("KRW-ETH")
+            target_price = get_target_price("{}".format(tk), 0.3)
+            current_price = get_current_price("{}".format(tk))
             if target_price < current_price and current_price < predicted_close_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-ETH", krw*0.9995)
+                    upbit.buy_market_order("{}".format(tk), krw*0.9995)
 
         else:
-            eth = get_balance("ETH")
+            eth = get_balance("{}".format(tka))
             if eth > 0.003:
-                upbit.sell_market_order("KRW-ETH", eth*0.9995)
+                upbit.sell_market_order("{}".format(tk), eth*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
