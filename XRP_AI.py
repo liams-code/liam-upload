9d2e77254a8b4637acbd351b300a64f8ffe0823d
liams-code <99477913+liams-code@users.noreply.github.com>
2022-08-15 19:29:18 +0900

    Update XRP_AI.py

--- XRP_AI.py
+++ XRP_AI.py
@@ -67,31 +67,33 @@ print("autotrade start")
 # 자동매매 시작 , XRP 5000원 이상으로 바꿈 , seconds을 hours=1로 바꿔서 8시에 매도하게 함.break, 10/3,0.3
 while True:
     try:
+        tk = "KRW-XRP"
+        tka = "XRP"
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-XRP")
+        start_time = get_start_time("{}".format(tk))
         end_time = start_time + datetime.timedelta(days=1)
         schedule.run_pending()
         
-        current_price = get_current_price("KRW-XRP")
-        avg_buy_price = get_avg_buy_price("KRW-XRP")
+        current_price = get_current_price("{}".format(tk))
+        avg_buy_price = get_avg_buy_price("{}".format(tk))
         if current_price > (avg_buy_price*1.1) or current_price < (avg_buy_price*0.97):
-            xrp = get_balance("XRP")
-            if xrp > 10:
-                upbit.sell_market_order("KRW-XRP", xrp*0.9995)
+            xrp = get_balance("{}".format(tka))
+            if xrp > 0.003:
+                upbit.sell_market_order("{}".format(tk), xrp*0.9995)
                 break
 
         if start_time < now < end_time - datetime.timedelta(hours=1):
-            target_price = get_target_price("KRW-XRP", 0.3)
-            current_price = get_current_price("KRW-XRP")
+            target_price = get_target_price("{}".format(tk), 0.3)
+            current_price = get_current_price("{}".format(tk))
             if target_price < current_price and current_price < predicted_close_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-XRP", krw*0.9995)
+                    upbit.buy_market_order("{}".format(tk), krw*0.9995)
 
         else:
-            xrp = get_balance("XRP")
-            if xrp > 10:
-                upbit.sell_market_order("KRW-XRP", xrp*0.9995)
+            xrp = get_balance("{}".format(tka))
+            if xrp > 0.003:
+                upbit.sell_market_order("{}".format(tk), xrp*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
